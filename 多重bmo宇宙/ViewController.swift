e4858aed7ad79e07b9e91a37452d21fecf234fff
hydee <hydee0209@hotmail.com>
2023-06-30 13:46:53 +0800

    Initial Commit

---
+++ 多重bmo宇宙/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  多重bmo宇宙
+//
+//  Created by Hydee Chen on 2023/6/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
